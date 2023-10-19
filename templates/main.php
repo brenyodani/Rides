e35b939beaf72c75ee6ba4b5b2761560acb3acdb
Patrick <pk@gerhardt.io>
2023-10-19 15:23:27 +0200

    added router

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,3 @@
 <?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Daniel Brenyo <brenyodani@gmail.com>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-?>
-<div id="content"></div>
+
+script('rides', 'rides-main');
