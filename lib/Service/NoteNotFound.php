0b37c4c55309eb8a5dd8f7a5ba457d331088278f
Patrick <pk@gerhardt.io>
2023-10-24 15:38:41 +0200

    deleted the unnecessary files and folders

--- lib/Service/NoteNotFound.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Daniel Brenyo <brenyodani@gmail.com>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace OCA\Rides\Service;
-
-class NoteNotFound extends \Exception {
-}
